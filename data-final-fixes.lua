c30325ad98f30111c352b04acb1b3a19f18de33c
Dmitriy <59513717+Dantorior@users.noreply.github.com>
2023-10-22 19:01:20 +0300

    Update 1.1.8

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,12 +1,19 @@
--- Define the new stack size
+-- New stack size
 local newStackSize = settings.startup["all-items-stacks"].value
+local newDefReqStackSize = settings.startup["all-items-stacks-def-req"].value
 
--- Define the table of item types to update
+-- Item types to update
 local itemTypes = {"item", "ammo", "capsule", "module", "tool", "repair-tool", "item-with-entity-data", "rail-planner", "rail-signal", "rail-chain-signal", "gun"}
 
 -- Loop through all items of each type and set their stack size
 for _, itemType in pairs(itemTypes) do
   for _, item in pairs(data.raw[itemType]) do
     item.stack_size = newStackSize
+	item.default_request_amount = newDefReqStackSize
   end
+end
+
+-- SE
+if mods["space-exploration"] then
+	data.raw.item["rocket-fuel"].stack_size = 10
 end
--- settings.lua
+++ settings.lua
@@ -1,11 +1,20 @@
 data:extend({
 	{
-        type = "int-setting",
-        name = "all-items-stacks",
+		type = "int-setting",
+		name = "all-items-stacks",
 		order = "aa",
-        setting_type = "startup",
+		setting_type = "startup",
 		default_value = 2000,
-        minimum_value = 1,
-        maximum_value = 100000,
-    }
+		minimum_value = 1,
+		maximum_value = 100000,
+    },
+	{
+		type = "int-setting",
+		name = "all-items-stacks-def-req",
+		order = "ab",
+		setting_type = "startup",
+		default_value = 1000,
+		minimum_value = 0,
+		maximum_value = 100000,
+	}
 })
