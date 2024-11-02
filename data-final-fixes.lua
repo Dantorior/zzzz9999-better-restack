5f8e854d45820fb0bbe601cfc03c2404a298ba14
ExexDiablo <59513717+Dantorior@users.noreply.github.com>
2024-11-02 02:36:04 +0200

    v.1.2.0

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,5 @@
+-- Stack size multiplier
+local stackMultiplier = settings.startup["stack-size-multiplier"].value
 -- New stack size
 local newStackSize = settings.startup["all-items-stacks"].value
 -- New default request size
@@ -21,27 +23,40 @@ function table.contains(table, element)
 	end
 	return false
 end
--- Loop through items and check flags
+
+-- Loop through items types and check flags
 for _, itemType in pairs(itemTypes) do
-    for _, item in pairs(data.raw[itemType]) do
-        local shouldChange = true
-        if item.flags then
-            for _, flag in ipairs(item.flags) do
-                if table.contains(possibleFlags, flag) then
-                    if flag == "not-stackable" then
-                        item.stack_size = 1
-                        shouldChange = false
-                        break
-                    end
-                end
-            end
-        end
-        if shouldChange then
-            item.stack_size = newStackSize
-            item.default_request_amount = newDefReqSize
-        end
-    end
+	for _, item in pairs(data.raw[itemType]) do
+		local shouldChange = true
+		if item.flags then
+			for _, flag in ipairs(item.flags) do
+				if table.contains(possibleFlags, flag) then
+					if flag == "not-stackable" then
+						item.stack_size = 1
+						shouldChange = false
+						break
+					end
+				end
+			end
+		end
+		if shouldChange then
+			-- If toggle true
+			if settings.startup["enable-stack-size-multiplier"].value then
+				if item.stack_size then
+					item.stack_size = item.stack_size * stackMultiplier
+					item.default_request_amount = newDefReqStackSize * stackMultiplier
+				end
+			-- If toggle false
+			else
+				if shouldChange then
+					item.stack_size = newStackSize
+					item.default_request_amount = newDefReqStackSize
+				end
+			end
+		end
+	end
 end
+
 -- SE
 if mods["space-exploration"] then
 	data.raw.item["rocket-fuel"].stack_size = 10
