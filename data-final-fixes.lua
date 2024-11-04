8876df5149b0d52461db37e3a02a9b7cce758ef7
ExexDiablo <59513717+Dantorior@users.noreply.github.com>
2024-11-04 02:37:57 +0200

    v.1.3.0.
    dlc support, item name, and weightmaster. :)

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,58 +1,118 @@
--- Stack size multiplier
-local stackMultiplier = settings.startup["stack-size-multiplier"].value
--- New stack size
-local newStackSize = settings.startup["all-items-stacks"].value
--- New default request size
-local newDefReqStackSize = settings.startup["all-items-stacks-def-req"].value
--- Item types to update
-local typesString = settings.startup["all-item-types"].value
--- Set of possible flags
+local stackMultiplier = settings.startup["sm-stack-size-multiplier"].value
+local newStackSize = settings.startup["sm-items-stacks"].value
+local newDefReqStackSize = settings.startup["sm-items-def-req"].value
+
 local possibleFlags = { "draw-logistic-overlay", "hidden", "always-show", "hide-from-bonus-gui", "hide-from-fuel-tooltip", "not-stackable", "can-extend-inventory", "primary-place-result", "mod-openable", "only-in-cursor", "spawnable" }
--- Set for item types
-local itemTypes = {}
--- Extracting individual types from the string
-for typeStr in typesString:gmatch("[^%s,]+") do
-    table.insert(itemTypes, typeStr)
+
+local function parseStringToTable(inputString)
+	local resultTable = {}
+	for item in inputString:gmatch("[^%s,]+") do
+		table.insert(resultTable, item)
+	end
+	return resultTable
 end
--- Check set return true/false
-function table.contains(table, element)
-	for _, value in ipairs(table) do
+
+local function tableContains(tbl, element)
+	for _, value in ipairs(tbl) do
 		if value == element then
 			return true
 		end
 	end
 	return false
 end
--- Loop through items types and check flags
-for _, itemType in pairs(itemTypes) do
-	if data.raw[itemType] then
-		for _, item in pairs(data.raw[itemType]) do
-			local shouldChange = true
-			
-			if item.flags then
-				for _, flag in ipairs(item.flags) do
-					if table.contains(possibleFlags, flag) then
-						if flag == "not-stackable" then
-							item.stack_size = 1
-							shouldChange = false
-							break
-						end
-					end
+
+local itemTypes = parseStringToTable(settings.startup["sm-item-types"].value)
+local itemNames = parseStringToTable(settings.startup["sm-item-names"].value)
+
+local function processItemFlags(item)
+    if item.flags then
+        for _, flag in ipairs(item.flags) do
+            if tableContains(possibleFlags, flag) then
+                if flag == "not-stackable" then
+                    item.stack_size = 1
+                    return false
+                end
+            end
+        end
+    end
+    return true
+end
+
+local function updateItemStackSize(item, useMultiplier, useNewStack)
+    if item.stack_size then
+        if useMultiplier then
+            item.stack_size = item.stack_size * stackMultiplier
+            item.default_request_amount = newDefReqStackSize * stackMultiplier
+        elseif useNewStack then
+            item.stack_size = newStackSize
+            item.default_request_amount = newDefReqStackSize
+        end
+    end
+end
+
+local function updateItemWeight(item)
+	if item.weight then
+		item.weight = item.weight / settings.startup["sm-weight"].value
+    end
+end
+
+if settings.startup["sm-enable-type"].value then
+    for _, itemType in pairs(itemTypes) do
+        if data.raw[itemType] then
+            for _, item in pairs(data.raw[itemType]) do
+                local shouldChange = processItemFlags(item)
+                if shouldChange then
+                    updateItemStackSize(item, true, false)
+                end
+            end
+        else
+            log("Warning: item type '" .. itemType .. "' does not exist.")
+        end
+    end
+end
+
+if settings.startup["sm-enable-name"].value then
+	for _, itemName in pairs(itemNames) do
+		local item = data.raw["item"] and data.raw["item"][itemName]
+		if item then
+			local shouldChange = processItemFlags(item)
+			if shouldChange then
+				if settings.startup["sm-enable-stack-size-multiplier"].value and not settings.startup["sm-enable-rewrite"].value then
+					updateItemStackSize(item, true, false)
+				elseif not settings.startup["sm-enable-stack-size-multiplier"].value and not settings.startup["sm-enable-rewrite"].value  then
+					updateItemStackSize(item, false, true)
+				elseif settings.startup["sm-enable-stack-size-multiplier"].value and settings.startup["sm-enable-rewrite"].value then
+					updateItemStackSize(item, false, true)
 				end
 			end
-			
-			if shouldChange and item.stack_size then
-				if settings.startup["enable-stack-size-multiplier"].value then
-					item.stack_size = item.stack_size * stackMultiplier
-					item.default_request_amount = newDefReqStackSize * stackMultiplier
+		else
+			log("Warning: item name '" .. itemName .. "' does not exist.")
+		end
+	end
+end
+if mods["space-age"] then
+	local weightTypes = parseStringToTable(settings.startup["sm-weight-item-types"].value)
+	local weightNames = parseStringToTable(settings.startup["sm-weight-item-names"].value)
+	if settings.startup["sm-enable-weight"].value then
+		if settings.startup["sm-if-weight"].value then
+			for _, weightTypes in pairs(weightTypes) do
+				local weightOfType = data.raw[weightTypes]
+				if weightOfType then
+					updateItemWeight(weightOfType)
 				else
-					item.stack_size = newStackSize
-					item.default_request_amount = newDefReqStackSize
+					log("Warning: item type '" .. weightTypes .. "' does not exist.")
+				end
+			end
+		elseif not settings.startup["sm-if-weight"].value then
+			for _, weightNames in pairs(weightNames) do
+				local weightOfName = data.raw["item"] and data.raw["item"][weightNames]
+				if weightOfName then
+					updateItemWeight(weightOfName)
+				else
+					log("Warning: item name '" .. weightNames .. "' does not exist.")
 				end
 			end
 		end
-	else
-		log("Warning: itemType '" .. itemType .. "' does not exist in data.raw.")
 	end
 end
 -- SE
--- settings.lua
+++ settings.lua
@@ -1,32 +1,32 @@
 data:extend({
 	{
         type = "bool-setting",
-        name = "enable-stack-size-multiplier",
+        name = "sm-enable-stack-size-multiplier",
 		order = "aa",
         setting_type = "startup",
-        default_value = false,
+        default_value = true,
 	},
 	{
 		type = "int-setting",
-		name = "stack-size-multiplier",
+		name = "sm-stack-size-multiplier",
 		order = "ab",
 		setting_type = "startup",
-		default_value = 1,
+		default_value = 5,
 		minimum_value = 1,
 		maximum_value = 1000,
     },
 	{
 		type = "int-setting",
-		name = "all-items-stacks-def-req",
+		name = "sm-items-def-req",
 		order = "ac",
 		setting_type = "startup",
-		default_value = 200,
+		default_value = 2,
 		minimum_value = 0,
 		maximum_value = 10000,
 	},
 	{
 		type = "int-setting",
-		name = "all-items-stacks",
+		name = "sm-items-stacks",
 		order = "ad",
 		setting_type = "startup",
 		default_value = 1000,
@@ -34,11 +34,83 @@ data:extend({
 		maximum_value = 10000,
     },
 	{
-		type = "string-setting",
-		name = "all-item-types",
+        type = "bool-setting",
+        name = "sm-enable-type",
 		order = "ae",
+        setting_type = "startup",
+        default_value = true,
+	},
+	{
+		type = "string-setting",
+		name = "sm-item-types",
+		order = "af",
 		setting_type = "startup",
 		default_value = "item, ammo, capsule, module, tool, repair-tool, item-with-entity-data, rail-planner, rail-signal, rail-chain-signal, gun",
 		allow_blank = true
+	},
+	{
+        type = "bool-setting",
+        name = "sm-enable-name",
+		order = "ag",
+        setting_type = "startup",
+        default_value = true,
+	},
+	{
+		type = "string-setting",
+		name = "sm-item-names",
+		order = "ah",
+		setting_type = "startup",
+		default_value = "barrel, iron-ore, copper-ore, coal, uranium-ore",
+		allow_blank = true
+	},
+	{
+        type = "bool-setting",
+        name = "sm-enable-rewrite",
+		order = "ai",
+        setting_type = "startup",
+        default_value = true,
 	}
 })
+if mods["space-age"] then
+	data:extend({
+		{
+			type = "bool-setting",
+			name = "sm-enable-weight",
+			order = "ba",
+			setting_type = "startup",
+			default_value = false,
+		},
+		{
+			type = "double-setting",
+			name = "sm-weight",
+			order = "bb",
+			setting_type = "startup",
+			default_value = 0.2,
+			minimum_value = 0.10,
+			maximum_value = 1000,
+		},
+		{
+			type = "bool-setting",
+			name = "sm-if-weight",
+			order = "bc",
+			setting_type = "startup",
+			default_value = true,
+		},
+		{
+			type = "string-setting",
+			name = "sm-weight-item-names",
+			order = "bd",
+			setting_type = "startup",
+			default_value = "barrel, iron-ore, copper-ore, coal, uranium-ore",
+			allow_blank = true
+		},
+		{
+			type = "string-setting",
+			name = "sm-weight-item-types",
+			order = "be",
+			setting_type = "startup",
+			default_value = "item, ammo, capsule, module, tool, repair-tool, item-with-entity-data, rail-planner, rail-signal, rail-chain-signal, gun",
+			allow_blank = true
+		}
+	})
+end
